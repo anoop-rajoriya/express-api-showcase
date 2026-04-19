c67a418e40366cf9fc92f4d861c647a7171934d5
anoop rajoriya <anooprajoriya9827@gmail.com>
2026-04-19 13:40:39 +0530

    feat: add jwt tokens utils

--- src/common/utils/crypto.js
+++ src/common/utils/crypto.js
@@ -4,9 +4,10 @@ export const generateHash = (payload)=>{
     return crypto.createHash("sha256").update(payload).digest("hex")
 }
 
-export const compareHash = ({userHash, dbHash})=>{
-    const buffA = Buffer.from(userHash, "hex")
-    const buffB = Buffer.from(dbHash, "hex")
+export const compareHash = (password, passwordHash)=>{
+    const ph = generateHash(password)
+    const buffA = Buffer.from(ph, "hex")
+    const buffB = Buffer.from(passwordHash, "hex")
     const match = buffA.length == buffB.length && crypto.timingSafeEqual(buffA, buffB)
 
     return match ? true : false
